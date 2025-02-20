b3846ae3d2f87f6df76a6c7b8cd9492e286cd2e5
Xihua Wang <2201174299@qq.com>
2025-02-21 04:44:25 +0800

    update engine --> auto create database

--- backend/db/engine.py
+++ backend/db/engine.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine
+import psycopg2
+
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker
 
 
@@ -8,9 +10,19 @@ DB_PASSWORD = "123456"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_NAME = "ide"
-DB_URL = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DB_URL, echo=True)
+DB_CONNECTION_URL = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}"
+DB_ADMIN_URL = f"{DB_CONNECTION_URL}/postgres"
+DB_DATABASE_URL = f"{DB_CONNECTION_URL}/{DB_NAME}"
+
+# 创建数据库
+try:
+    with create_engine(DB_ADMIN_URL, isolation_level="AUTOCOMMIT").connect() as conn:
+        conn.execute(text(f"CREATE DATABASE {DB_NAME}"))
+except psycopg2.errors.DuplicateDatabase:
+    pass
+# 连接目标数据库
+engine = create_engine(DB_DATABASE_URL, echo=True)
 session = sessionmaker(bind=engine)
 
 
