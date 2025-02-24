6e4e4be5d2b3f4ae618d2ec24ff30f5cdfa1d333
Xihua Wang <2201174299@qq.com>
2025-02-24 17:15:12 +0800

    change encrypt key

--- backend/util/encrypt.py
+++ backend/util/encrypt.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from fastapi import Header, HTTPException, status
 from typing import Optional
 
-from backend.config import ENCRYPT_JWT_ALGO, ENCRYPT_JWT_KEY
+from backend.config import ENCRYPT_JWT_ALGO, ENCRYPT_KEY
 
 
 def jwt_encode(data: dict, exp_hours: Optional[int]) -> str:
@@ -21,7 +21,7 @@ def jwt_encode(data: dict, exp_hours: Optional[int]) -> str:
     data = data.copy()
     if exp_hours:
         data["exp"] = datetime.now() + timedelta(hours=exp_hours)
-    return jwt.encode(data, ENCRYPT_JWT_KEY, ENCRYPT_JWT_ALGO)
+    return jwt.encode(data, ENCRYPT_KEY, ENCRYPT_JWT_ALGO)
 
 
 def jwt_verify(token: str = Header(None, alias="Access-Token")) -> dict:
@@ -40,6 +40,6 @@ def jwt_verify(token: str = Header(None, alias="Access-Token")) -> dict:
     try:
         if token is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Missing token")
-        return jwt.decode(token, ENCRYPT_JWT_KEY, ENCRYPT_JWT_ALGO)
+        return jwt.decode(token, ENCRYPT_KEY, ENCRYPT_JWT_ALGO)
     except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
