0e93ffd8236ab27368ed5ac4fb7f37b53897807a
fsht12345 <1298383171@qq.com>
2025-03-17 02:48:43 +0800

    update

--- backend/intellide/routers/__init__.py
+++ backend/intellide/routers/__init__.py
@@ -2,15 +2,18 @@ from fastapi import APIRouter
 
 from intellide.routers.course import api as course_api
 from intellide.routers.course_chat import ws as course_chat_ws
+from intellide.routers.course_collaborative_directory_entry import ws as course_collaborative_directory_entry_ws
 from intellide.routers.course_directory import api as course_directory_api
 from intellide.routers.course_directory_entry import api as course_directory_entry_api
 from intellide.routers.course_student import api as course_student_api
+from intellide.routers.course_collaborative_directory_entry import api as course_collaborative_directory_entry_api
 from intellide.routers.user import api as user_api
 
 # 创建 WebSocket 路由
 router_ws = APIRouter(prefix="/ws")
 
 router_ws.include_router(course_chat_ws)
+router_ws.include_router(course_collaborative_directory_entry_ws)
 
 # 创建 API 路由
 router_api = APIRouter(prefix="/api")
@@ -18,6 +21,7 @@ router_api = APIRouter(prefix="/api")
 router_api.include_router(course_api)
 router_api.include_router(course_directory_api)
 router_api.include_router(course_directory_entry_api)
+router_api.include_router(course_collaborative_directory_entry_api)
 router_api.include_router(course_student_api)
 router_api.include_router(user_api)
 
