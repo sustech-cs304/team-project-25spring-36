fb02e4b530f554b22afb3190fad8c0e52e454e7f
Lenovo <12212910@mail.sustech.edu.cn>
2025-05-25 05:39:46 +0800

    修改笔记部分

--- frontend/intelligent-ide/src/notebook/NotebookSerializer.ts
+++ frontend/intelligent-ide/src/notebook/NotebookSerializer.ts
@@ -3,22 +3,28 @@ import * as vscode from 'vscode';
 export class MyNotebookSerializer implements vscode.NotebookSerializer {
   async deserializeNotebook(content: Uint8Array, _token: vscode.CancellationToken): Promise<vscode.NotebookData> {
     const rawContent = new TextDecoder().decode(content);
-    const cells = JSON.parse(rawContent).cells.map((cell: any) => new vscode.NotebookCellData(
-      cell.kind === 'code' ? vscode.NotebookCellKind.Code : vscode.NotebookCellKind.Markup,
-      cell.value,
-      cell.language
+    const parsed = JSON.parse(rawContent);
+
+    const cells = parsed.cells.map((cell: any) => new vscode.NotebookCellData(
+      cell.cell_type === 'code' ? vscode.NotebookCellKind.Code : vscode.NotebookCellKind.Markup,
+      cell.source.join(''),
+      cell.cell_type === 'code' ? cell.metadata.language : 'markdown'
     ));
+
     return new vscode.NotebookData(cells);
   }
 
   async serializeNotebook(data: vscode.NotebookData, _token: vscode.CancellationToken): Promise<Uint8Array> {
     const rawContent = JSON.stringify({
       cells: data.cells.map(cell => ({
-        kind: cell.kind === vscode.NotebookCellKind.Code ? 'code' : 'markup',
-        value: cell.value,
-        language: cell.languageId
+        cell_type: cell.kind === vscode.NotebookCellKind.Code ? 'code' : 'markdown',
+        source: [cell.value],
+        metadata: {
+          language: cell.languageId
+        }
       }))
     });
+
     return new TextEncoder().encode(rawContent);
   }
 }
